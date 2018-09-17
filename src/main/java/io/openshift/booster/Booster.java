dcb3301428aa78c1e60b00d999e5178307cf8ba1
Adam Kowalewski <test@localhost>
2018-09-17 16:05:19 +0000

    Commit 2

--- src/main/java/io/openshift/booster/Booster.java
+++ src/main/java/io/openshift/booster/Booster.java
@@ -1,4 +1,4 @@
-package io.openshift.booster;                                                      
+Ttttpackage io.openshift.booster;                                                      
 public class Booster {                                                      
  public static void main(String[] args) {
      System.out.println("Hello World!");
